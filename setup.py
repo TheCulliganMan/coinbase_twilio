30104d4980eda0c7fa8363a4fb5fc0282ffe3a61
Ryan Culligan <rrculligan@gmail.com>
2017-03-03 11:14:25 -0600

    submitted to the python package index
    small changes

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ import os
 from setuptools import setup
 
 REQUIREMENTS = [
-    line.strip() for line in open(os.path.join(os.path.dirname(__file__),
-                                               'requirements.txt')).readlines()]
+    line.strip() for line in open('requirements.txt').readlines()]
 
 setup(name='etherwatch',
       packages=['etherwatch'],
-      version='0.1',
+      version='0.1.1',
       description='Notifies you about ether prices.',
-      url='http://github.com/theculliganman/ether_watch',
-      author='Ryan',
+      author = 'Ryan Culligan',
       author_email='rrculligan@gmail.com',
       license='MIT',
       install_requires=REQUIREMENTS,
+      url='http://github.com/theculliganman/ether_watch',
       download_url='https://codeload.github.com/TheCulliganMan/ether_watch/archive/master.tar.gz',
       keywords = ['ether', 'ethereum', 'coinbase'],
+      classifiers = [],
 )
