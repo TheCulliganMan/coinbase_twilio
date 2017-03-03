1285baba53a9387dc43aa0c0f41686b91a166f3d
Ryan Culligan <rrculligan@gmail.com>
2017-03-03 11:25:34 -0600

    update requirements

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-import os
 from setuptools import setup
-
-REQUIREMENTS = [
-    line.strip() for line in open('requirements.txt').readlines()]
 
 setup(name='etherwatch',
       packages=['etherwatch'],
-      version='0.1.1',
+      version='0.1.4',
       description='Notifies you about ether prices.',
       author = 'Ryan Culligan',
       author_email='rrculligan@gmail.com',
       license='MIT',
-      install_requires=REQUIREMENTS,
+      install_requires=[
+        'coinbase',
+        'twilio',
+        'slackclient',
+      ],
       url='http://github.com/theculliganman/ether_watch',
       download_url='https://codeload.github.com/TheCulliganMan/ether_watch/archive/master.tar.gz',
       keywords = ['ether', 'ethereum', 'coinbase'],
